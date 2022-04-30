537598edf3abaac85d25405039c0254f0854bdf8
yangzii0920 <11530441+yangzii0920@users.noreply.github.com>
2022-04-29 21:36:41 -0400

    clean up

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,12 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "@/components/HelloWorld.vue";
 </script>
 
 <template>
   <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
+    <div >
      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/acnh">ACNH sorter</RouterLink>
+        <RouterLink to="/sort">ACNH sorter</RouterLink>
       </nav>
     </div>
   </header>
